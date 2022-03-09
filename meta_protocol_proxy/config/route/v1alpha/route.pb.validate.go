072484f5528cac177a91b4493514743648e729f8
Huabing Zhao <zhaohuabing@gmail.com>
2022-03-09 17:22:47 +0800

    support header mutation
    Signed-off-by: Huabing Zhao <zhaohuabing@gmail.com>

--- meta_protocol_proxy/config/route/v1alpha/route.pb.validate.go
+++ meta_protocol_proxy/config/route/v1alpha/route.pb.validate.go
@@ -15,7 +15,7 @@ import (
 	"time"
 	"unicode/utf8"
 
-	"google.golang.org/protobuf/types/known/anypb"
+	"github.com/golang/protobuf/ptypes"
 )
 
 // ensure the imports are used
@@ -30,7 +30,7 @@ var (
 	_ = time.Duration(0)
 	_ = (*url.URL)(nil)
 	_ = (*mail.Address)(nil)
-	_ = anypb.Any{}
+	_ = ptypes.DynamicAny{}
 )
 
 // Validate checks the field values on RouteConfiguration with the rules
@@ -151,6 +151,50 @@ func (m *Route) Validate() error {
 				cause:  err,
 			}
 		}
+	}
+
+	if len(m.GetRequestMutation()) > 1000 {
+		return RouteValidationError{
+			field:  "RequestMutation",
+			reason: "value must contain no more than 1000 item(s)",
+		}
+	}
+
+	for idx, item := range m.GetRequestMutation() {
+		_, _ = idx, item
+
+		if v, ok := interface{}(item).(interface{ Validate() error }); ok {
+			if err := v.Validate(); err != nil {
+				return RouteValidationError{
+					field:  fmt.Sprintf("RequestMutation[%v]", idx),
+					reason: "embedded message failed validation",
+					cause:  err,
+				}
+			}
+		}
+
+	}
+
+	if len(m.GetResponseMutation()) > 1000 {
+		return RouteValidationError{
+			field:  "ResponseMutation",
+			reason: "value must contain no more than 1000 item(s)",
+		}
+	}
+
+	for idx, item := range m.GetResponseMutation() {
+		_, _ = idx, item
+
+		if v, ok := interface{}(item).(interface{ Validate() error }); ok {
+			if err := v.Validate(); err != nil {
+				return RouteValidationError{
+					field:  fmt.Sprintf("ResponseMutation[%v]", idx),
+					reason: "embedded message failed validation",
+					cause:  err,
+				}
+			}
+		}
+
 	}
 
 	return nil
@@ -378,3 +422,88 @@ var _ interface {
 	Cause() error
 	ErrorName() string
 } = RouteActionValidationError{}
+
+// Validate checks the field values on KeyValue with the rules defined in the
+// proto definition for this message. If any rules are violated, an error is returned.
+func (m *KeyValue) Validate() error {
+	if m == nil {
+		return nil
+	}
+
+	if utf8.RuneCountInString(m.GetKey()) < 1 {
+		return KeyValueValidationError{
+			field:  "Key",
+			reason: "value length must be at least 1 runes",
+		}
+	}
+
+	if len(m.GetKey()) > 16384 {
+		return KeyValueValidationError{
+			field:  "Key",
+			reason: "value length must be at most 16384 bytes",
+		}
+	}
+
+	if len(m.GetValue()) > 16384 {
+		return KeyValueValidationError{
+			field:  "Value",
+			reason: "value length must be at most 16384 bytes",
+		}
+	}
+
+	return nil
+}
+
+// KeyValueValidationError is the validation error returned by
+// KeyValue.Validate if the designated constraints aren't met.
+type KeyValueValidationError struct {
+	field  string
+	reason string
+	cause  error
+	key    bool
+}
+
+// Field function returns field value.
+func (e KeyValueValidationError) Field() string { return e.field }
+
+// Reason function returns reason value.
+func (e KeyValueValidationError) Reason() string { return e.reason }
+
+// Cause function returns cause value.
+func (e KeyValueValidationError) Cause() error { return e.cause }
+
+// Key function returns key value.
+func (e KeyValueValidationError) Key() bool { return e.key }
+
+// ErrorName returns error name.
+func (e KeyValueValidationError) ErrorName() string { return "KeyValueValidationError" }
+
+// Error satisfies the builtin error interface
+func (e KeyValueValidationError) Error() string {
+	cause := ""
+	if e.cause != nil {
+		cause = fmt.Sprintf(" | caused by: %v", e.cause)
+	}
+
+	key := ""
+	if e.key {
+		key = "key for "
+	}
+
+	return fmt.Sprintf(
+		"invalid %sKeyValue.%s: %s%s",
+		key,
+		e.field,
+		e.reason,
+		cause)
+}
+
+var _ error = KeyValueValidationError{}
+
+var _ interface {
+	Field() string
+	Reason() string
+	Key() bool
+	Cause() error
+	ErrorName() string
+} = KeyValueValidationError{}
