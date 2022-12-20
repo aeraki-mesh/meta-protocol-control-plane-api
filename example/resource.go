c7dbd9f7918fdd1aadeecd2b5eadae4e299e3127
zhaohuabing <zhaohuabing@gmail.com>
2022-12-20 14:30:00 +0800

    fix go package issue
    Signed-off-by: zhaohuabing <zhaohuabing@gmail.com>

--- example/resource.go
+++ example/resource.go
@@ -14,7 +14,7 @@
 package example
 
 import (
-	metaroute "github.com/aeraki-mesh/meta-protocol-control-plane-api/meta_protocol_proxy/config/route/v1alpha"
+	metaroute "github.com/aeraki-mesh/meta-protocol-control-plane-api/aeraki/meta_protocol_proxy/config/route/v1alpha"
 	httproute "github.com/envoyproxy/go-control-plane/envoy/config/route/v3"
 	"github.com/envoyproxy/go-control-plane/pkg/cache/types"
 	"github.com/envoyproxy/go-control-plane/pkg/cache/v3"
