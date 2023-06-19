a6deb4136cb7f247d8ebf39d262c02ce0543dac2
Huabing Zhao <zhaohuabing@gmail.com>
2023-06-19 01:47:46 +0000

    support multiplexing
    Signed-off-by: Huabing Zhao <zhaohuabing@gmail.com>

--- aeraki/meta_protocol_proxy/v1alpha/meta_protocol_proxy.pb.validate.go
+++ aeraki/meta_protocol_proxy/v1alpha/meta_protocol_proxy.pb.validate.go
@@ -234,6 +234,35 @@ func (m *MetaProtocolProxy) validate(all bool) error {
 		}
 	}
 
+	if all {
+		switch v := interface{}(m.GetProtocol()).(type) {
+		case interface{ ValidateAll() error }:
+			if err := v.ValidateAll(); err != nil {
+				errors = append(errors, MetaProtocolProxyValidationError{
+					field:  "Protocol",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		case interface{ Validate() error }:
+			if err := v.Validate(); err != nil {
+				errors = append(errors, MetaProtocolProxyValidationError{
+					field:  "Protocol",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		}
+	} else if v, ok := interface{}(m.GetProtocol()).(interface{ Validate() error }); ok {
+		if err := v.Validate(); err != nil {
+			return MetaProtocolProxyValidationError{
+				field:  "Protocol",
+				reason: "embedded message failed validation",
+				cause:  err,
+			}
+		}
+	}
+
 	switch m.RouteSpecifier.(type) {
 
 	case *MetaProtocolProxy_Rds:
@@ -1054,3 +1083,147 @@ var _ interface {
 	Cause() error
 	ErrorName() string
 } = TracingValidationError{}
+
+// Validate checks the field values on ApplicationProtocol with the rules
+// defined in the proto definition for this message. If any rules are
+// violated, the first error encountered is returned, or nil if there are no violations.
+func (m *ApplicationProtocol) Validate() error {
+	return m.validate(false)
+}
+
+// ValidateAll checks the field values on ApplicationProtocol with the rules
+// defined in the proto definition for this message. If any rules are
+// violated, the result is a list of violation errors wrapped in
+// ApplicationProtocolMultiError, or nil if none found.
+func (m *ApplicationProtocol) ValidateAll() error {
+	return m.validate(true)
+}
+
+func (m *ApplicationProtocol) validate(all bool) error {
+	if m == nil {
+		return nil
+	}
+
+	var errors []error
+
+	if utf8.RuneCountInString(m.GetName()) < 1 {
+		err := ApplicationProtocolValidationError{
+			field:  "Name",
+			reason: "value length must be at least 1 runes",
+		}
+		if !all {
+			return err
+		}
+		errors = append(errors, err)
+	}
+
+	if all {
+		switch v := interface{}(m.GetCodec()).(type) {
+		case interface{ ValidateAll() error }:
+			if err := v.ValidateAll(); err != nil {
+				errors = append(errors, ApplicationProtocolValidationError{
+					field:  "Codec",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		case interface{ Validate() error }:
+			if err := v.Validate(); err != nil {
+				errors = append(errors, ApplicationProtocolValidationError{
+					field:  "Codec",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		}
+	} else if v, ok := interface{}(m.GetCodec()).(interface{ Validate() error }); ok {
+		if err := v.Validate(); err != nil {
+			return ApplicationProtocolValidationError{
+				field:  "Codec",
+				reason: "embedded message failed validation",
+				cause:  err,
+			}
+		}
+	}
+
+	// no validation rules for Multiplexing
+
+	if len(errors) > 0 {
+		return ApplicationProtocolMultiError(errors)
+	}
+
+	return nil
+}
+
+// ApplicationProtocolMultiError is an error wrapping multiple validation
+// errors returned by ApplicationProtocol.ValidateAll() if the designated
+// constraints aren't met.
+type ApplicationProtocolMultiError []error
+
+// Error returns a concatenation of all the error messages it wraps.
+func (m ApplicationProtocolMultiError) Error() string {
+	var msgs []string
+	for _, err := range m {
+		msgs = append(msgs, err.Error())
+	}
+	return strings.Join(msgs, "; ")
+}
+
+// AllErrors returns a list of validation violation errors.
+func (m ApplicationProtocolMultiError) AllErrors() []error { return m }
+
+// ApplicationProtocolValidationError is the validation error returned by
+// ApplicationProtocol.Validate if the designated constraints aren't met.
+type ApplicationProtocolValidationError struct {
+	field  string
+	reason string
+	cause  error
+	key    bool
+}
+
+// Field function returns field value.
+func (e ApplicationProtocolValidationError) Field() string { return e.field }
+
+// Reason function returns reason value.
+func (e ApplicationProtocolValidationError) Reason() string { return e.reason }
+
+// Cause function returns cause value.
+func (e ApplicationProtocolValidationError) Cause() error { return e.cause }
+
+// Key function returns key value.
+func (e ApplicationProtocolValidationError) Key() bool { return e.key }
+
+// ErrorName returns error name.
+func (e ApplicationProtocolValidationError) ErrorName() string {
+	return "ApplicationProtocolValidationError"
+}
+
+// Error satisfies the builtin error interface
+func (e ApplicationProtocolValidationError) Error() string {
+	cause := ""
+	if e.cause != nil {
+		cause = fmt.Sprintf(" | caused by: %v", e.cause)
+	}
+
+	key := ""
+	if e.key {
+		key = "key for "
+	}
+
+	return fmt.Sprintf(
+		"invalid %sApplicationProtocol.%s: %s%s",
+		key,
+		e.field,
+		e.reason,
+		cause)
+}
+
+var _ error = ApplicationProtocolValidationError{}
+
+var _ interface {
+	Field() string
+	Reason() string
+	Key() bool
+	Cause() error
+	ErrorName() string
+} = ApplicationProtocolValidationError{}
